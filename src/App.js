409dfedfbffe9b17d05659b82a2b6c508afbff21
saurav <saurav>
2020-07-28 22:23:00 +0530

    Built a simple pomodoro app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from 'react';
+import './App.scss';
 
 function App() {
+  const [timeLeft, setTimeLeft] = useState(25 * 60);
+  const [title, setTitle] = useState('Lets the countdown Begin!!');
+  const intervalRef = useRef(null);
+
+  function startTimer() {
+    console.log(intervalRef.current)
+    if (intervalRef.current !== null){
+      return;
+    }
+    setTitle(`You're doing great`);
+    intervalRef.current = setInterval(() => {
+      setTimeLeft(timeLeft => {
+        if (timeLeft >= 1) {
+          return timeLeft - 1;
+        }
+        else return 0;
+      });
+    }, 1000);
+
+  }
+
+
+  function resetTimer() {
+    clearInterval(intervalRef.current);
+    setTitle('Are you ready for another round?');
+    setTimeLeft(25 * 60);
+  }
+
+  function stopTimer() {
+    if (intervalRef.current === null) return;
+    clearInterval(intervalRef.current);
+    intervalRef.current = null;
+    setTitle('Keep it up');
+  }
+
+
+  const minutes = Math.floor(timeLeft / 60).toString().padStart(2, '0');
+  const second = (timeLeft - minutes * 60).toString().padStart(2, '0');
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>{title}</h2>
+      <div className="timer">
+        <span>{minutes}</span>
+        <span>:</span>
+        <span>{second}</span>
+      </div>
+      <div className="buttons">
+        <button onClick={startTimer}>start</button>
+        <button onClick={stopTimer}>Stop</button>
+        <button onClick={resetTimer}>Reset</button>
+      </div>
     </div>
   );
 }
